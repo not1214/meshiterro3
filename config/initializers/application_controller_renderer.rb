51be118c65fff20c2bac8a0939d5a52ba751deff
not1214 <91748221+not1214@users.noreply.github.com>
2021-11-01 22:45:20 +0900

    Update index

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '869e1825d3e3f42d1ccf39bc50b4467b389e22499c651362ceaff140bdcd6c18eae14226daaaeb8a56804281f1db5b10940aa0721a21ac304e4cce2cf87303b7'
